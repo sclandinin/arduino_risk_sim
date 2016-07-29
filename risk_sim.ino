de6abb6b5969c383270f37235294c74d40405776
sclandinin <sclandinin@gmail.com>
2016-07-29 17:17:53 -0600

    Update risk_sim.ino

--- risk_sim.ino
+++ risk_sim.ino
@@ -32,6 +32,13 @@ int def_count = 2;
 void setup() {    
   
   Serial.begin(9600);
+  
+    // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+  
   Serial.print("*****Risk Dice Rolling Simulator*****\n\r\n\r");
   
   // initialize the digital pins
